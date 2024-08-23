b65d7369d0890476b9567d24540c09606a0c5599
mekanicas <brunomicelio52@gmail.com>
2024-08-23 13:56:13 -0300

    adding socket.io middleware and realtimeproducts view

--- src/app.js
+++ src/app.js
@@ -1,31 +1,59 @@
-import express from 'express';
-import {productsRouter} from './routes/productsRouter.js';
-import {cartRouter} from './routes/cartRouter.js';
-
-import handlebars from 'express-handlebars'
-import __dirname from './utils.js'
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import path from "path";
+import { productsRouter } from "./routes/productsRouter.js";
+import { cartRouter } from "./routes/cartRouter.js";
+import handlebars from "express-handlebars";
+import __dirname from "./utils.js";
+import { ProductsManager } from "./dao/productsManager.js";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 const PORT = 8080;
 
-app.engine('handlebars', handlebars.engine());
-app.set('views', __dirname+'/views');
-app.set('view engine', 'handlebars')
-app.use(express.static(__dirname+'public'))
-
+// Configuración del motor de plantillas Handlebars
+app.engine("handlebars", handlebars.engine());
+app.set("views", path.join(__dirname, "views")); 
+app.set("view engine", "handlebars");
+app.use(express.static(path.join(__dirname, "public")));
+// Middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use("/api/products", productsRouter)
-app.use("/api/cart", cartRouter)
+app.use(express.urlencoded({ extended: true }));
+// Rutas
+app.use("/api/products", productsRouter);
+app.use("/api/cart", cartRouter);
+// Rutas de vistas
+app.get("/", (req, res) => {
+  let testUser = {
+    name: "Bruno",
+    last_name: "Miceli",
+  };
+  res.render("index", testUser);
+});
+app.get("/realtimeproducts", (req, res) => {
+  res.render("realTimeProducts");
+});
+//Configuracion websockets
+let products = []; // Array para los products
 
-app.get("/" ,(req,res) => {
-    let testUser = {
-        name : "Bruno",
-        last_name : "Miceli"
-    }
-    res.render('index', testUser)
-})
+io.on("connection", (socket) => {
+  console.log("Usuario conectado");
 
-const server = app.listen(PORT, () => {
-    console.log(`Server escuchando en el puerto ${PORT}`);
-})
+  socket.on("new-product", async (product) => {
+    await ProductsManager.addProducto(product);
+    const updatedProducts = await ProductsManager.getProducts(
+      "./src/data/products.json"
+    );
+    io.emit("update-products", updatedProducts);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Usuario desconectado");
+  });
+});
+// Iniciar el servidor
+server.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
